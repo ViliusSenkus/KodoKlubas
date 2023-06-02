ddfed939c2fc31360aff75f9c8d62bdd423e8d47
ViliusSenkus <vilius.s@outlook.com>
2023-06-02 18:47:09 +0300

    .

--- task7.js
+++ task7.js
@@ -1,7 +1,5 @@
-// let target = 2022;
-// let numbers = [7, 3, 21, 12, 13, 48, 43, 31, 15, 2, 33];
-let target = 10;
-let numbers = [1,2,3];
+let target = 2022;
+let numbers = [7, 3, 21, 12, 13, 48, 43, 31, 15, 2, 33];
 
 function countWays(target, numbers) {
   let dp = new Array(target + 1).fill(0);
@@ -9,13 +7,13 @@ function countWays(target, numbers) {
 
   for (let i = 0; i < numbers.length; i++) {
     for (let j = numbers[i]; j <= target; j++) {
-      console.log(dp);
-      console.log("prieš tai: i - ", i, " j - ",j, "number[i] - ", numbers[i], "dp[j] - ", dp[j], )
-      console.log("skaiciuos kad dp[", j, "] skaicius", dp[j], " = dp[j]",dp[j],"+ dp [j", j ," - num[i]", numbers[i],"]");
+      // console.log(dp);
+      // console.log("prieš tai: i - ", i, " j - ",j, "number[i] - ", numbers[i], "dp[j] - ", dp[j], )
+      // console.log("skaiciuos kad dp[", j, "] skaicius", dp[j], " = dp[j]",dp[j],"+ dp [j", j ," - num[i]", numbers[i],"]");
       dp[j] += dp[j - numbers[i]];
-      console.log("po to: i - ", i, " j - ",j, "number[i] - ", numbers[i], "dp[j] - ", dp[j], )
-      console.log(dp);
-      console.log(dp[-1]);
+      // console.log("po to: i - ", i, " j - ",j, "number[i] - ", numbers[i], "dp[j] - ", dp[j], )
+      // console.log(dp);
+      // console.log(dp[-1]);
     }
   }
 
